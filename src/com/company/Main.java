220dcb2d28b2ca7af28dff79967f53ff6183ad08
Jessica Qin <jessicaq319@gmail.com>
2017-01-26 22:16:34 -0500

    Final Final Product

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Option 1: Convert from RGB triplet to hexadecimal\nOption 2: Convert from hexadecimal to RGB triplet\nPlease pick an option");
+        System.out.println("Please pick an option\nOption 1: Convert from RGB triplet to hexadecimal\nOption 2: Convert from hexadecimal to RGB triplet");
         Scanner sc = new Scanner(System.in);
         String in = sc.nextLine();
 
@@ -16,29 +16,51 @@ public class Main {
             if (in.equalsIgnoreCase("option 1")
                     || in.equalsIgnoreCase("option one")
                     || in.equalsIgnoreCase("RGB to hex")
-                    || in.equalsIgnoreCase("RGB triplet to hexadecimal"))
+                    || in.equalsIgnoreCase("RGB triplet to hexadecimal")
+                    || in.equalsIgnoreCase("1"))
             {
                 System.out.println("please enter an RGB triplet and seperate each color value with a comma");
                 Scanner sc2 = new Scanner(System.in);
                 String color = sc2.nextLine();
 
-                Convert.RGBtoHex(color);
-                break;
+                try
+                {
+                    Convert.RGBtoHex(color);
+                    break;
+                }
+                catch (ArrayIndexOutOfBoundsException e)
+                {
+
+                }
+                catch (NumberFormatException e)
+                {
+
+                }
+
 
             }
             else if (in.equalsIgnoreCase("option 2")
                     || in.equalsIgnoreCase("option two")
                     || in.equalsIgnoreCase("hex to RGB")
-                    || in.equalsIgnoreCase("hexadecimal to RGB triplet"))
+                    || in.equalsIgnoreCase("hexadecimal to RGB triplet")
+                    || in.equals("2"))
 
             {
 
-                System.out.println("Please enter a hexadecimal");
+                System.out.println("Please enter a hexadecimal color");
                 Scanner sc2 = new Scanner(System.in);
                 String color = sc2.nextLine();
 
-                Convert.hexToRGB(color);
-                break;
+                try
+                {
+                    Convert.hexToRGB(color);
+                    break;
+                }
+                catch (StringIndexOutOfBoundsException e)
+                {
+                    System.out.println("This is not a hexadecimal");
+                }
+
 
 
             }
